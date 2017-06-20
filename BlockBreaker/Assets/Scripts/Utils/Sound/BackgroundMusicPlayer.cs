0955bf20ac82c2188187c944b94d6d41ddfc20b8
Pavel Szypka <p.szypka@gmail.com>
2017-06-20 17:08:52 +0200

    Init

---
+++ BlockBreaker/Assets/Scripts/Utils/Sound/BackgroundMusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusicPlayer : MonoBehaviour {
+
+    static BackgroundMusicPlayer instance = null;
+
+   private AudioSource _audioSource;
+
+   void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+}
